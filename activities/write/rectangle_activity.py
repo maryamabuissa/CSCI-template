181caf5d1963fa4bbac8b174e8e1bb2b88c2e1ec
Maryam <maryam@abuissa.net>
2026-09-10 13:29:20 -0600

    Print rectangle activity

---
+++ activities/write/rectangle_activity.py
@@ -0,0 +1,15 @@
+# Fill this in and add unit tests
+
+def rectangle(w, h, c):
+    # print character
+
+    # print a line (print c w times)
+
+    # do that h times
+
+def line(w, c):
+    # print c w times
+
+
+r = rectangle(2, 3, "y")
+print(r)
