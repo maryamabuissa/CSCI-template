6c0484296c2b2641469ba60e3abfb82e67b3a3c9
Maryam <maryam@abuissa.net>
2026-09-22 13:26:56 -0600

    Add fizzbuzz loop activity

---
+++ activities/reorder/loop.py
@@ -0,0 +1,20 @@
+# Reorder the following lines of code so that it prints 0 through n
+# unless the number is divisible by x, then print fizz
+# or if it's divisible by y, then print Buzz
+# or if it's divisible by both, print Fizzbuzz
+
+
+n = 10
+y = 2
+x = 3
+
+    elif i % x == 0:        # line a
+        print("Buzz")       # line b
+    if i % (x * y) == 0:    # line c
+    else:                   # line d
+        print("Fizz")       # line e
+        print("Fizzbuzz")   # line f
+for i in range(n):          # line g
+    elif i % y == 0:        # line h
+        print(i)            # line k
+
