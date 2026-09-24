7341456353708bfae577183f519cb29323c3acb1
Maryam <maryam@abuissa.net>
2026-09-24 13:01:18 -0600

    Activity from class

---
+++ activities/write/number_guessing_game.py
@@ -0,0 +1,19 @@
+# Implement a game where 
+# 1) you pick a random number
+# 2) the user can guess a number until they get it right
+# 3) (optional) you can give hints
+
+def check_input(guess):
+    return False
+
+def main():
+    # pick a random int
+    # each turn, take an input, and check it
+    while True:
+        # take input
+        i = ""
+        # check input
+        correct = check_input(i)
+
+if __name__ == "__main__":
+    main()
